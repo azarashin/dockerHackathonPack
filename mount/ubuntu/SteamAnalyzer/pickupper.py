f0d7b41cd1a7c9dec524b58c3dcc6f08d606131f
azarashin <azarashin1979@gmail.com>
2021-11-10 19:34:29 +0900

    データ抽出機能を更新（月情報の取りこぼしを改善）

--- mount/ubuntu/SteamAnalyzer/pickupper.py
+++ mount/ubuntu/SteamAnalyzer/pickupper.py
@@ -106,7 +106,9 @@ class ProductInfo:
 
 
     def init_releaseDate(self, body):
-        mp = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12 }
+        mp = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12 
+            , 'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8
+            , 'September':9, 'October':10, 'November':11, 'December':12 }
         p_release = re.compile(r'<b>Release Date:</b> ([0-9]+?) ([^0-9]+?), ([0-9]+?)<br>', flags=re.DOTALL)
         m = p_release.search(body)
         if m:
