6bda8c6c00c15385f71261f94109469060b2554d
azarashin <azarashin1979@gmail.com>
2022-01-22 03:21:20 +0900

    定期実行スクリプトを更新。

--- dockerfile/ubuntu/daily_task.sh
+++ dockerfile/ubuntu/daily_task.sh
@@ -1,9 +1,20 @@
+echo "Daily task start." > /data/last_update.log
+date >> /data/last_update.log
+
+echo "Start StockAnalyzer." >> /data/last_update.log
 cd /data/StockAnalyzer
 python3 analyzer.py
 
+echo "Finish StockAnalyzer." >> /data/last_update.log
+date >> /data/last_update.log
+
+echo "Start SteamAnalyzer" >> /data/last_update.log
+
 cd /data/SteamAnalyzer
-python3 packer.py
+python3 packer.py && python3 database_updator.py
+
+echo "Finish SteamAnalyzer" >> /data/last_update.log
+date >> /data/last_update.log
 
 
-echo xxx >> data.txt
 
